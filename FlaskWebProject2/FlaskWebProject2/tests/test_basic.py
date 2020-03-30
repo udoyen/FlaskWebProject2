8183c93eada68455e61d25a73a5c1629e05ea8b6
george <datameshprojects@gmail.com>
2020-03-30 06:44:24 +0100

    added flask test units

---
+++ FlaskWebProject2/FlaskWebProject2/tests/test_basic.py
@@ -0,0 +1,24 @@
+import unittest
+import requests
+import FlaskWebProject2
+
+
+class TestBasic(unittest.TestCase):
+    def setUp(self):
+        FlaskWebProject2.app.testing = True
+        self.app = FlaskWebProject2.app.test_client()
+
+    def test_homepage(self):
+        response = self.app.get('/', follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_rootpage(self):
+        response = self.app.get('/home', follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
+    def test_contactpage(self):
+        response = self.app.get('/contact', follow_redirects = True)
+        self.assertEqual(response.status_code, 200)
+
+if __name__ == '__main__':
+    unittest.main()
